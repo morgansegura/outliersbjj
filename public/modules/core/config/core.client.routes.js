6f0c3d75a64eabf214bf72e17c014aace3d71eee
Morgan Segura <morgansegura@gmail.com>
2016-05-17 07:05:09 -0700

    add controllers and views for additional one off pages

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,6 +11,24 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		}).state('classes', {
+			url: '/classes',
+			templateUrl: 'modules/core/views/classes.client.view.html'
+		}).state('instructors', {
+			url: '/instructors',
+			templateUrl: 'modules/core/views/instructors.client.view.html'
+		}).state('schedules', {
+			url: '/schedules',
+			templateUrl: 'modules/core/views/schedule.client.view.html'
+		}).state('about', {
+			url: '/about',
+			templateUrl: 'modules/core/views/about.client.view.html'
+		}).state('waiver', {
+			url: '/waiver',
+			templateUrl: 'modules/core/views/waiver.client.view.html'
+		}).state('contact', {
+			url: '/contact',
+			templateUrl: 'modules/core/views/contact.client.view.html'
 		});
 	}
 ]);
